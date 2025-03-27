1d2b4900a44edeec32b71fb11fbdc141fc18d74f
tehila <t0527135298@gmail.com>
2025-03-27 15:01:59 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using TodoApi;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ToDoDbContext>(
+    options => options.UseMySql(
+        builder.Configuration.GetConnectionString("ToDoDB"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ToDoDB"))));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapGet("/items", (ToDoDbContext db) => Results.Ok(db.Items.ToList()));
+
+app.MapPost("/items", (Item newItem, ToDoDbContext db) =>
+{
+    db.Items.Add(newItem);
+    db.SaveChanges();
+    return Results.Created($"/items/{newItem.Id}", newItem);
+});
+
+app.MapPut("/items/{id}", (int id, Item updatedItem, ToDoDbContext db) =>
+{
+    var existingItem = db.Items.Find(id);
+    if (existingItem == null)
+    {
+        return Results.NotFound();
+    }
+    if(updatedItem.Name!=null)
+        existingItem.Name = updatedItem.Name;
+    existingItem.IsComplete = updatedItem.IsComplete;
+    db.SaveChanges();
+    return Results.Ok(existingItem);
+});
+
+app.MapDelete("/items/{id}", (int id, ToDoDbContext db) =>
+{
+    var item = db.Items.Find(id);
+    if (item == null)
+    {
+        return Results.NotFound();
+    }
+    db.Items.Remove(item);
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+app.Run();
